234345f06b0bcee37c43390af174804be06e0d69
Sakshamnayak123 <sakshamnayaksakshamnayak@gmail.com>
2025-02-13 21:57:39 +0530

    git remote add origin https://github.com/Sakshamnayak123/JS-work.git

---
+++ first.js
@@ -0,0 +1,19 @@
+// let radius = parseInt(prompt("Enter radius of circle"))
+// console.log(22/7*radius*radius);
+
+let a = parseInt(prompt("Enter first number"))
+let b = parseInt(prompt("Enter second number "))
+
+console.log("Bfore swaping");
+console.log("a=",a);
+console.log("b=",b);
+
+let c=a;
+a=b;
+b=c;
+
+console.log("\n After swapping");
+console.log("a=",a);
+console.log("b=",b);
+
+
